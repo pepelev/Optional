854ba631982e78879049d021893a0349c6c57eb6
pepelev <pepelev@skbkontur.ru>
2023-09-02 01:45:01 +0500

    Suppress nullability on Option create

--- src/Optional/Option.cs
+++ src/Optional/Option.cs
@@ -18,13 +18,13 @@
         /// <param name="value">The value to be wrapped.</param>
         /// <returns>An optional containing the specified value.</returns>
         public static Option<T, TException> Some<T, TException>(T value) =>
-            new(value, default, true);
+            new(value, exception: default!, hasValue: true);
 
         /// <summary>
         ///     Creates an empty Option&lt;T&gt; instance.
         /// </summary>
         /// <returns>An empty optional.</returns>
-        public static Option<T> None<T>() => new(default, false);
+        public static Option<T> None<T>() => new(value: default!, hasValue: false);
 
         /// <summary>
         ///     Creates an empty Option&lt;T, TException&gt; instance,
@@ -33,6 +33,6 @@
         /// <param name="exception">The exceptional value.</param>
         /// <returns>An empty optional.</returns>
         public static Option<T, TException> None<T, TException>(TException exception) =>
-            new(default, exception, false);
+            new(value: default!, exception, hasValue: false);
     }
 }
